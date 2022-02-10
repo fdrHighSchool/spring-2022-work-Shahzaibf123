edf1a21824108c8234d3b4b9d3897c72a2be8484
Shahzaib Fareed <shahzaibf@nycstudents.net>
2022-02-10 08:48:47 -0500

    classes

--- student-data/Datapplic.java
+++ student-data/Datapplic.java
@@ -1,23 +1,23 @@
 import java.util.*;
 public class Datapplic {
   public static void main(String args[]) {
-    /*store data
-    int[] ID = {1234567,9876543,1357911,2468101, 1928374};
-    String[] Name = {"Jimmy Smith", "Mara Hernandez", "Jamal Jenkins", "Rana Ah", "Michael Go"};
-    Boolean[] CC = {true,false,false,true,true};
-    String[] Grades = {"90,80,95,90,100", "95,100,100,90,90","85,85,95,100,90","90,100,95,85,90","80,80,65,55,90"};
+    //store data
+    int[] id = {1234567,9876543,1357911,2468101, 1928374};
+    String[] name = {"Jimmy Smith", "Mara Hernandez", "Jamal Jenkins", "Rana Ah", "Michael Go"};
+    Boolean[] cc = {true,false,false,true,true};
+    String[] grades = {"90,80,95,90,100", "95,100,100,90,90","85,85,95,100,90","90,100,95,85,90","80,80,65,55,90"};
     for (int i = 0; i < ID.length;i++){
-      System.out.println(ID[i]  + ": " + Name[i] + " has grades of " + Grades[i]);
-      if (CC[i]){
+      System.out.println(id[i]  + ": " + name[i] + " has grades of " + grades[i]);
+      if (cc[i]){
         System.out.println("Was a close contact!");
       }
       else{
         System.out.println("Was not a close contact!");
       }
 
-      plan: 4 different arrays with each index connecting
-      Ex:Jimmy Smith is index 1 in array 2, with ID is index 1 in array 1, etc.
-    */
-    
+    /*  plan: 4 different arrays with each index connecting
+      Ex:Jimmy Smith is index 1 in array 2, with ID is index 1 in array 1, etc. */
+
+
   }
 }
--- student-data/Student.java
+++ student-data/Student.java
@@ -4,6 +4,35 @@ public class Student {
   private String name;
   private boolean closeContact;
   private int[] grades;
-  
 
+  //constructor method
+  public Student(String id, String n, boolean cc, integer g) {
+    this.id = id;
+    this.name = n;
+    this.cc = cc;
+    this.grades = g;
+  }
+
+
+  //behaviors (methods)
+
+  //get closecontact status of student
+  public boolean getCloseContact() {
+
+  }
+
+  //set the value of close contact
+  public void setCloseContact(boolean value) {
+
+  }
+
+  //get the String value of the object
+  public String getName() {
+
+  }
+
+  //see if the student is failing
+  public boolean isFailing() {
+
+  }
 }
