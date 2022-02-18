ec9a381b7b22d22f5c949d6040a01249323ac7c5
Shahzaib Fareed <shahzaibf@nycstudents.net>
2022-02-18 08:34:37 -0500

    Metrocard started

--- untitled folder/MetroCard.java
+++ untitled folder/MetroCard.java
@@ -3,11 +3,28 @@ public class MetroCard {
   private int usesleft;
 
 }
-  public Metrocard(String id,){
+  public Metrocard(String id){
     this.id = id;
     this.usesleft = 0;
   }
   public Metrocard(String id, int usesleft){
     this.id = id;
     this.usesleft = usesleft;
+  }
+
+  public int Adduse(){
+    return this.usesleft + 1;
+  }
+
+  public int useuse(){
+    return this.uses - 1;
+  }
+
+  public boolean Enoughuses(){
+    if (usesleft == 0){
+      return false;
+    }
+    else{
+      return true;
+    }
   }
