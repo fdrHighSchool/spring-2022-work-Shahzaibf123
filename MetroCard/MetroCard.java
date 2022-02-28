c93670e4123a6c3920c6b0c911ef705e766a30f8
Shahzaib Fareed <shahzaibf@nycstudents.net>
2022-02-28 08:27:34 -0500

    Metrocard,did on repl

--- MetroCard/MetroCard.java
+++ MetroCard/MetroCard.java
@@ -1,4 +1,55 @@
-public class MetroCard {
+public class Metrocard {
+  private String id;
+  private int moneyleft;
+  private int Currenttime;
 
+  public Metrocard(String id){
+    this.id = id;
+    this.moneyleft = 0;
+    this.Currenttime = 900;
+  }
 
+  public Metrocard(String id, int moneyleft){
+    this.id = id;
+    this.moneyleft = moneyleft;
+    this.Currenttime = 900;
+  }
+  public Metrocard(String id, int moneyleft, int Currenttime){
+    this.id = id;
+    this.moneyleft = moneyleft;
+    this.Currenttime = Currenttime;
+  }
+
+  public String doubleuse(){
+    return "Can not be swiped within 20-minute windows";
+  }
+  public boolean Canuse(){ //timeoriented
+    if (Currenttime > 530 && Currenttime < 2030){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  public int Adduse(){
+    return this.moneyleft + 275;
+  }
+
+  public int useuse(){
+    if (Canuse()){
+    return this.moneyleft - 275;
+    }
+    else {
+      return this.moneyleft;//nothing happens, card wasnt used
+    }
+  }
+
+  public boolean Enoughuses(){//money-oriented
+    if (moneyleft < 275){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
 }
--- MetroCard/Tester.java
+++ MetroCard/Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
   public static void main(String args[]) {
-  
+    MetroCard metrocard1 = new MetroCard("11111",3);
+    System.out.println(metrocard1.printInfo());
   }
 }
