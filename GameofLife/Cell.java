8e278fd1533a15feca697ef48c77efd7481ec9ec
Shahzaibf123 <shahzaibf@nycstudents.net>
2022-03-14 18:41:32 -0400

    GoL

--- GameofLife/Cell.java
+++ GameofLife/Cell.java
@@ -2,24 +2,25 @@ import java.util.*;
 public class Cell{
 
   private int DoA;
-  private String[][] board;
 
   public Cell(int DoA){
     this.DoA = DoA;
-    this.board = board;
 
   }
-  
+
   public int getStatus(){
     return DoA;
   }
 
 
-  public int Neighbors(int x, int y){
-    count = 0;
-    if ()
+  public String toString() {
+    if(this.status == 0) {
+      return "X";
+    }
+    else {
+      return "O";
+    }
   }
-
 
 
 
--- GameofLife/Driver.java
+++ GameofLife/Driver.java
@@ -2,43 +2,17 @@ public class Driver{//start of class Driver
   public static void main(String[] args){//start of main
     //alive cell X, dead cell O, nothing blank
 
-    int generation = 1;
-    String[][] board = createBoard();
-    board = setBoard(board);
-    displayBoard(board);
+   Grid world1 = new Grid(10, 10);
+   System.out.println(world1);
+   System.out.println(world1.countNeighbors(8, 8)); // 0
+   System.out.println(world1.countNeighbors(2, 5));
 
   }//end of main
 
 
 
 
-  public static String[][] setBoard(String [][] board){
-    for (int row = 0; row < 10; row++){
-      for (int col = 0; col < 10; col++){
-        board[row][col] = (Math.random() > .5) ? "X" : "";
-      }
-    }
-    return board;
-  }
 
-  public static String[][] createBoard(){ //createBoard function started
-    String[][] board = new String[10][10];
-    for (int row = 0; row < 10; row++){
-      for (int col = 0; col < 10; col++){
-        board[row][col] = "";
-      }
-    }
-    return board;
-  }//createBoard function ended
-
-  public static void displayBoard(String board[][]){//displayBoard function started
-    for(int row = 0; row < board.length; row++) {
-      for(int col = 0; col < board[row].length; col++) {
-        System.out.print(board[row][col] + " ");
-      }
-    System.out.println();
-    }
-  }//displayBoard function ended
 
 
 }//end of class Driver
